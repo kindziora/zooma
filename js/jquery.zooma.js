e78bcbde58fa64f18c8a7f989cc29be635aa3aa2
root <kindziora@live.de>
2013-08-28 18:21:14 +0200

    bootstrap 3

--- js/jquery.zooma.js
+++ js/jquery.zooma.js
@@ -2,16 +2,60 @@
  zooma Author Alexander Kindziora 2013 https://github.com/kindziora
  ========================================================================== */
 (function($) {
+    var ContainerWidth = $('.zoomcontainer').width(), ContainerHeight = $('.zoomcontainer').height();
+
+    var methods = {
+    };
+
+    methods.zoomIn = function($el, options) {
+        options.bodyX = (screen.width / 2) - options.elCenterX
+                , options.bodyY = (screen.height / 2) - options.elCenterY;
+
+        options.body.addClass('zooma');
+        $el.addClass('zoomed');
+        $el.parent().append($('<div class="zoombackground"></div>'));
+
+        var stylesBackground = [
+            'top:' + (-options.bodyY) + 'px',
+            'left:' + (-options.bodyX) + 'px',
+            'height:' + screen.height + 'px',
+            'width:' + screen.width + 'px',
+            'transition: ' + options.speed + 'ms opacity ease-in-out'
+        ];
+
+        $('.zoombackground').attr('style', stylesBackground.join(';'));
+
+        window.setTimeout(function() {
+            $('.zoombackground').addClass('show');
+            options.shown.call($el);
+        }, options.speed);
+    };
+
+    methods.zoomOut = function($el, options) {
+        $('.zoombackground').removeClass('show');
+        options.factor = 'none';
+        options.body.removeClass('zooma');
+        $el.removeClass('zoomed');
+        options.body.addClass('fixed');
+
+        window.setTimeout(function() {
+            $('.zoombackground').remove();
+            options.closed.call($el);
+            options.body.removeClass('fixed');
+        }, options.speed);
+    };
+
     $.fn.zooma = function(options) {
-        var $el = this
-                , $body = $('.zoomcontainer')
-                , bodyX = 0
-                , bodyY = 0
-                , elCenterX = 0
-                , elCenterY = 0
-                , defaultOptions = {
-            'speed': $el.attr('data-speed') || 250,
-            'factor': $el.attr('data-factor') || 1.5,
+
+        var $el = this,
+                defaultOptions = {
+            'speed': $el.attr('data-speed') || 200,
+            'factor': $el.attr('data-factor') || 1.4,
+            'body': $('.zoomcontainer'),
+            'bodyX': 0,
+            'bodyY': 0,
+            'elCenterX': $el.offset().left - $('.zoomcontainer').offset().left + ($el.width() / 2),
+            'elCenterY': $el.offset().top - $('.zoomcontainer').offset().top + ($el.height() / 2),
             'shown': function() {
                 var $el = this;
                 /*var stylesZoomedEl = [
@@ -27,58 +71,25 @@
 
         options = $.extend(true, defaultOptions, options);
 
-
-        if ($body.hasClass('zooma')) {
-
-            options.factor = 'none';
-            $body.removeClass('zooma');
-            $el.removeClass('zoomed');
-
-            $('.zoombackground').removeClass('show');
-
-            window.setTimeout(function() {
-                $('.zoombackground').remove();
-                options.closed.call($el);
-            }, options.speed);
+        if (!options.body.hasClass('zooma')) {
+            methods.zoomIn($el, options);
         } else {
-
-            elCenterX = $el.offset().left - $body.offset().left + ($el.width() / 2)
-                    , elCenterY = $el.offset().top - $body.offset().top + ($el.height() / 2)
-                    , bodyX = (screen.width / 2) - elCenterX
-                    , bodyY = (screen.height / 2) - elCenterY;
-
-            $body.addClass('zooma');
-            $el.addClass('zoomed');
-            $el.parent().append($('<div class="zoombackground"></div>'));
-
-            var stylesBackground = [
-                'top:' + (-bodyY) + 'px',
-                'left:' + (-bodyX) + 'px',
-                'height:' + screen.height + 'px',
-                'width:' + screen.width + 'px',
-                'transition: ' + options.speed + 'ms opacity ease-in-out'
-            ];
-
-            $('.zoombackground').attr('style', stylesBackground.join(';'));
-
-            window.setTimeout(function() {
-                $('.zoombackground').addClass('show');
-                options.shown.call($el);
-            }, options.speed);
-
+            methods.zoomOut($el, options);
         }
 
         var stylesBody = [
             'transition-duration: ' + options.speed + 'ms',
-            'left:' + bodyX + 'px',
-            'top:' + bodyY + 'px',
-            'transform:scale(' + options.factor + ')',
-            'transform-origin: ' + (elCenterX) + 'px ' + (elCenterY) + 'px',
-            '-webkit-transform:scale(' + options.factor + ')',
-            '-webkit-transform-origin: ' + (elCenterX) + 'px ' + (elCenterY) + 'px'
+            'left:' + options.bodyX + 'px',
+            'top:' + options.bodyY + 'px',
+            'width:' + ContainerWidth + 'px',
+            'height:' + ContainerHeight + 'px',
+            'transform: scale(' + options.factor + ')',
+            'transform-origin: ' + (options.elCenterX) + 'px ' + (options.elCenterY) + 'px',
+            '-webkit-transform: scale(' + options.factor + ')',
+            '-webkit-transform-origin: ' + (options.elCenterX) + 'px ' + (options.elCenterY) + 'px'
         ];
 
-        $body.attr('style', stylesBody.join(';'));
+        options.body.attr('style', stylesBody.join(';'));
 
 
     };
